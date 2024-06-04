f8e8f77f185e7c61bf860b73d270e788252f52d0
pinal-pansuriya <pinal24vp@gmail.com>
2024-06-04 18:25:11 +0530

    add

---
+++ index.js
@@ -0,0 +1,19 @@
+let submit = document.getElementById('submit')
+let bar = document.getElementById('bar')
+
+function downloadBtn() {
+    alert("cv downloaded")
+    window.open('resume.html')
+}
+
+let ul = document.querySelector('ul')
+
+function barr() {
+    ul.classList.toggle('showData')
+    if (ul.classList == 'showData') {
+        bar.className = 'bx bx-x'
+    } else {
+        bar.className = 'bx bx-menu'
+    }
+}
+
